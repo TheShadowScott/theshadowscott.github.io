4dab62818b444cc71bdaaa3d280d1c5fbe8cd667
Scott Pendland <smpendland@outlook.com>
2024-03-07 12:18:31 -0500

    Added Major Nations (minors started_

--- The Ixil Isles/index.js
+++ The Ixil Isles/index.js
@@ -25,6 +25,37 @@ function includeHTML() {
         }
     }
 }
+function createNationDisplayBlock(element, name, population, headOfState, capital, headOfGovernment, govType, highCrimes, military, found, religion) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                // cast response text to HTML because XMLResponse DOESNT FUCKING WORK
+                var parser = new DOMParser();
+                var htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+                if (htmlDoc === null) {
+                    return;
+                }
+                htmlDoc.getElementById("nation-name").innerText = name;
+                htmlDoc.getElementById("population").innerText = "~".concat(population.toLocaleString("en-UK"));
+                htmlDoc.getElementById("head-state").innerText = headOfState;
+                htmlDoc.getElementById("capital").innerText = capital;
+                htmlDoc.getElementById("head-gov").innerText = headOfGovernment !== null && headOfGovernment !== void 0 ? headOfGovernment : "N/A";
+                htmlDoc.getElementById("government-type").innerText = govType;
+                htmlDoc.getElementById("high-crimes").innerHTML = highCrimes.join(", ");
+                htmlDoc.getElementById("military").innerText = military;
+                htmlDoc.getElementById("founding").innerText = found;
+                htmlDoc.getElementById("religion").innerHTML = religion;
+                element.appendChild(htmlDoc.body);
+            }
+            else {
+                element.innerHTML = "Page not found.";
+            }
+        }
+    };
+    xhttp.open("GET", "/The%20Ixil%20Isles/Nations/NationStatblock.tbl.html", true);
+    xhttp.send();
+}
 includeHTML();
 setTimeout(function () {
     var collapsibles = document.querySelectorAll('.collapse');
@@ -40,4 +71,41 @@ setTimeout(function () {
             });
         });
     });
-}, 500);
+}, 100);
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        // Get the collapsible elements
+        var factionsCollapse = document.querySelector('#factions-collapse');
+        var locationsCollapse = document.querySelector('#locations-collapse');
+        var minorCollapse = document.querySelector('#minor-collapse'); // Added this line
+        // If the stored state exists, apply it
+        if (localStorage.getItem('factionsCollapse') === 'show') {
+            new bootstrap.Collapse(factionsCollapse, { toggle: false }).show();
+        }
+        if (localStorage.getItem('locationsCollapse') === 'show') {
+            new bootstrap.Collapse(locationsCollapse, { toggle: false }).show();
+        }
+        if (localStorage.getItem('minorCollapse') === 'show') { // Added this block
+            new bootstrap.Collapse(minorCollapse, { toggle: false }).show();
+        }
+        // When a collapsible is shown or hidden, store its state
+        factionsCollapse.addEventListener('show.bs.collapse', function () {
+            localStorage.setItem('factionsCollapse', 'show');
+        });
+        factionsCollapse.addEventListener('hide.bs.collapse', function () {
+            localStorage.setItem('factionsCollapse', 'hide');
+        });
+        locationsCollapse.addEventListener('show.bs.collapse', function () {
+            localStorage.setItem('locationsCollapse', 'show');
+        });
+        locationsCollapse.addEventListener('hide.bs.collapse', function () {
+            localStorage.setItem('locationsCollapse', 'hide');
+        });
+        minorCollapse.addEventListener('show.bs.collapse', function () {
+            localStorage.setItem('minorCollapse', 'show');
+        });
+        minorCollapse.addEventListener('hide.bs.collapse', function () {
+            localStorage.setItem('minorCollapse', 'hide');
+        });
+    }, 150);
+});
--- The Ixil Isles/index.ts
+++ The Ixil Isles/index.ts
@@ -1,3 +1,7 @@
+type nullable<T> = undefined | null | T;
+
+type service = "Compulsory" | "Voluntary" | "None";
+
 function includeHTML()
 {
     let z: string | any[] | HTMLCollectionOf<Element>,
@@ -28,23 +32,103 @@ function includeHTML()
     }
 }
 
+function createNationDisplayBlock(element: HTMLDivElement, name: string, population: number,
+    headOfState: string, capital: string, headOfGovernment: nullable<string>,
+    govType: string, highCrimes: Array<string>, military: service, found: string, religion: string)
+{
+    let xhttp: XMLHttpRequest = new XMLHttpRequest();
+    xhttp.onreadystatechange = function ()
+    {
+        if (this.readyState === 4)
+        {
+            if (this.status === 200)
+            {
+                // cast response text to HTML because XMLResponse DOESNT FUCKING WORK
+                let parser = new DOMParser();
+                let htmlDoc = parser.parseFromString(this.responseText, 'text/html');
+                if (htmlDoc === null) { return; }
+                (htmlDoc.getElementById("nation-name") as HTMLElement).innerText = name;
+                (htmlDoc.getElementById("population") as HTMLElement).innerText = `~${population.toLocaleString("en-UK")}`;
+                (htmlDoc.getElementById("head-state") as HTMLElement).innerText = headOfState;
+                (htmlDoc.getElementById("capital") as HTMLElement).innerText = capital;
+                (htmlDoc.getElementById("head-gov") as HTMLElement).innerText = headOfGovernment ?? "N/A";
+                (htmlDoc.getElementById("government-type") as HTMLElement).innerText = govType;
+                (htmlDoc.getElementById("high-crimes") as HTMLElement).innerHTML = highCrimes.join(", ");
+                (htmlDoc.getElementById("military") as HTMLElement).innerText = military;
+                (htmlDoc.getElementById("founding") as HTMLElement).innerText = found;
+                (htmlDoc.getElementById("religion") as HTMLElement).innerHTML = religion;
+                element.appendChild(htmlDoc.body);
+            }
+            else { element.innerHTML = "Page not found."; }
+        }
+    };
+    xhttp.open("GET", "/The%20Ixil%20Isles/Nations/NationStatblock.tbl.html", true);
+    xhttp.send();
+}
+
 includeHTML();
 
 
 setTimeout(() =>
 {
     const collapsibles = document.querySelectorAll('.collapse');
-
-    collapsibles.forEach(function (collapsible) {
+    collapsibles.forEach(function (collapsible)
+    {
         // Listen for the 'show' event
-        collapsible.addEventListener('show.bs.collapse', function () {
+        collapsible.addEventListener('show.bs.collapse', function ()
+        {
             // When a collapsible is about to be shown, hide all others
-            collapsibles.forEach(function (c) {
-                if (c !== collapsible) {
-                    var bsCollapse = new bootstrap.Collapse(c, {toggle: false});
+            collapsibles.forEach(function (c : Element)
+            {
+                if (c !== collapsible)
+                {
+                    var bsCollapse = new bootstrap.Collapse(c, { toggle: false });
                     bsCollapse.hide();
                 }
             });
         });
     });
-}, 500)
+}, 100);
+
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+    setTimeout(() =>
+    {
+        // Get the collapsible elements
+        var factionsCollapse = document.querySelector('#factions-collapse');
+        var locationsCollapse = document.querySelector('#locations-collapse');
+        var minorCollapse = document.querySelector('#minor-collapse'); // Added this line
+
+        // If the stored state exists, apply it
+        if (localStorage.getItem('factionsCollapse') === 'show') {
+            new bootstrap.Collapse(factionsCollapse, {toggle: false}).show();
+        }
+        if (localStorage.getItem('locationsCollapse') === 'show') {
+            new bootstrap.Collapse(locationsCollapse, {toggle: false}).show();
+        }
+        if (localStorage.getItem('minorCollapse') === 'show') { // Added this block
+            new bootstrap.Collapse(minorCollapse, {toggle: false}).show();
+        }
+
+        // When a collapsible is shown or hidden, store its state
+        factionsCollapse.addEventListener('show.bs.collapse', function () {
+            localStorage.setItem('factionsCollapse', 'show');
+        });
+        factionsCollapse.addEventListener('hide.bs.collapse', function () {
+            localStorage.setItem('factionsCollapse', 'hide');
+        });
+        locationsCollapse.addEventListener('show.bs.collapse', function () {
+            localStorage.setItem('locationsCollapse', 'show');
+        });
+        locationsCollapse.addEventListener('hide.bs.collapse', function () {
+            localStorage.setItem('locationsCollapse', 'hide');
+        });
+        minorCollapse.addEventListener('show.bs.collapse', function () { // Added this block
+            localStorage.setItem('minorCollapse', 'show');
+        });
+        minorCollapse.addEventListener('hide.bs.collapse', function () { // Added this block
+            localStorage.setItem('minorCollapse', 'hide');
+        });
+    }, 150)
+});
